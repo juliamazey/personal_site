32d89c922e15ddf357b19ff62cb1d58497cdd2f2
Julia Marco <marcootj@gmail.com>
2019-01-14 13:51:01 -0700

    Added test_it_can_indicate_error_when_page_does_not_exist

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_it_can_indicate_error_when_page_does_not_exist
+
+    visit '//'
+    assert_equal 404, page.status_code
+    assert page.has_content?("Page doesn't exist")
+  end
 end
